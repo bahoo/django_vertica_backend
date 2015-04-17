fc7e2bb9a0e0e1c954403882a0ee8a2a7db3aa39
Frozen <phpconf@gmail.com>
2015-04-17 12:53:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email='zimbler@gmail.com',
     description='Vertica backend for Django',
     install_requires=[
-        'vertica-python>=0.2.4,<0.3'
+        'vertica-python>=0.2.4'
     ]
 )
